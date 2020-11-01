00af417d65014ca9058803893468509d6b7ed2a5
wjr21 <wjr21@sussex.ac.uk>
2020-11-01 21:05:16 +0000

    first commit with DM animation script

---
+++ swift_dm_animate.py
@@ -0,0 +1,94 @@
+#!/cosma/home/dp004/dc-rope1/.conda/envs/flares-env/bin/python
+import matplotlib as ml
+ml.use('Agg')
+import numpy as np
+from sphviewer.tools import QuickView, cmaps
+import matplotlib.pyplot as plt
+import sys
+from guppy import hpy; h=hpy()
+import os
+from swiftsimio import load
+import unyt
+
+
+def get_normalised_image(img, vmin=None, vmax=None):
+
+    if vmin == None:
+        vmin = np.min(img)
+    if vmax == None:
+        vmax = np.max(img)
+
+    img = np.clip(img, vmin, vmax)
+    img = (img - vmin) / (vmax - vmin)
+
+    return img
+
+
+def getimage(poss, hsml):
+
+    print('There are', poss.shape[0], 'dark matter particles in the region')
+
+    # Set up particle objects
+    R = QuickView(poss, hsml=hsml, mass=np.ones(poss.shape[0]), plot=False,
+                  r=15, p=0, t=0, ogscale=True)
+
+    img = R.get_image()
+
+    vmax = img.max()
+    vmin = vmax * 0.5
+
+    # Get colormaps
+    cmap = cmaps.twilight()
+
+    # Convert images to rgb arrays
+    rgb = cmap(get_normalised_image(img, vmin=vmin))
+
+    return rgb, R.get_extent()
+
+
+def single_frame(num):
+
+    snap = "%05d" % num
+
+    # Define path
+    path = '/cosma/home/dp004/dc-rope1/cosma7/SWIFT/DMO_test/data/mega_dmo_test_' + snap + ".hdf5"
+
+    data = load(path)
+
+    meta = data.metadata
+    boxsize = meta.boxsize
+
+    print(boxsize)
+
+    print(data.metadata.dark_matter_properties.field_names)
+
+    # # Define anchors dict for camera parameters
+    # anchors = {}
+    # anchors['sim_times'] = [0.0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    # anchors['id_frames'] = [0, 45, 188, 210, 232, 375, 420, 500]
+    # anchors['id_targets'] = [0, 'pass', 2, 'pass', 'pass', 'pass', 'pass', 0]
+    # anchors['r'] = [boxsize * 3 / 4, 'pass', boxsize / 100, 'same', 'pass', 'pass', 'pass', boxsize * 3 / 4]
+    # anchors['t'] = [0, 'pass', 'pass', -180, 'pass', -270, 'pass', -360]
+    # anchors['p'] = [0, 'pass', 'pass', 'pass', 'pass', 'pass', 'pass', 360 * 3]
+    # anchors['zoom'] = [1., 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    # anchors['extent'] = [10, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    #
+    # # Define the camera trajectory
+    # data = camera_tools.get_camera_trajectory(targets, anchors)
+
+    poss = data.dark_matter.coordinates
+
+    # Get images
+    rgb_DM, extent = getimage(poss, hsmls)
+
+    fig = plt.figure(figsize=(4, 4))
+    ax = fig.add_subplot(111)
+
+    ax.imshow(rgb_DM, extent=extent, origin='lower')
+    ax.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False,
+                   labeltop=False, labelright=False, labelbottom=False)
+
+    fig.savefig('plots/Ani/DM_animation_' + snap + '.png',
+                bbox_inches='tight')
+    plt.close(fig)
+
