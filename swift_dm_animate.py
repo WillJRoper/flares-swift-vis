b231cb1e89af41974a58a43d82a896dc29bc7983
wjr21 <wjr21@sussex.ac.uk>
2020-11-02 17:58:19 +0000

    angled cube

--- swift_dm_animate.py
+++ swift_dm_animate.py
@@ -81,8 +81,8 @@ def single_frame(num, max_pixel, nframes):
     anchors['id_frames'] = np.linspace(0, nframes, 8, dtype=int)
     anchors['id_targets'] = [0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['r'] = [boxsize.value + 1, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
-    anchors['t'] = [-35, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
-    anchors['p'] = [5, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    anchors['t'] = [0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    anchors['p'] = [35, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['zoom'] = [1., 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['extent'] = [10, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
 
