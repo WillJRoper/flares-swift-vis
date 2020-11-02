ad5d67dd0839392c20e40e2ba4aa0f192e869e5c
wjr21 <wjr21@sussex.ac.uk>
2020-11-02 17:49:37 +0000

    angled cube

--- swift_dm_animate.py
+++ swift_dm_animate.py
@@ -93,7 +93,7 @@ def single_frame(num, max_pixel, nframes):
     hsmls = data.dark_matter.softenings.value
 
     # Get images
-    rgb_DM, extent = getimage(cam_data, poss, hsmls, max_pixel, num)
+    rgb_DM, extent = getimage(cam_data, poss, hsmls, num, max_pixel)
 
     fig = plt.figure(figsize=(4, 4))
     ax = fig.add_subplot(111)
