e77092caf7a26c0b8edea601499d1cb771c0c560
wjr21 <wjr21@sussex.ac.uk>
2020-11-02 17:52:49 +0000

    angled cube

--- swift_dm_animate.py
+++ swift_dm_animate.py
@@ -37,8 +37,8 @@ def getimage(data, poss, hsml, num, max_pixel):
     S = sph.Scene(P)
 
     i = data[num]
-    i['xsize'] = 5000
-    i['ysize'] = 5000
+    i['xsize'] = 500
+    i['ysize'] = 500
     i['roll'] = 0
     S.update_camera(**i)
     R = sph.Render(S)
@@ -80,8 +80,8 @@ def single_frame(num, max_pixel, nframes):
     anchors['sim_times'] = [0.0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['id_frames'] = np.linspace(0, nframes, 8, dtype=int)
     anchors['id_targets'] = [0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
-    anchors['r'] = [boxsize.value + 0.5, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
-    anchors['t'] = [35, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    anchors['r'] = [boxsize.value + 1, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
+    anchors['t'] = [-35, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['p'] = [5, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['zoom'] = [1., 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['extent'] = [10, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
