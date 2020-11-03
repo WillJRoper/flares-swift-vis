e7bf78549f7198c65aea2651e5c4485bbded7e59
wjr21 <wjr21@sussex.ac.uk>
2020-11-03 11:40:07 +0000

    angled cube

--- swift_dm_animate.py
+++ swift_dm_animate.py
@@ -63,7 +63,7 @@ def single_frame(num, max_pixel, nframes):
     snap = "%04d" % num
 
     # Define path
-    path = '/cosma/home/dp004/dc-rope1/cosma7/SWIFT/DMO_1380/data/mega_dmo_test_' + snap + ".hdf5"
+    path = '/cosma/home/dp004/dc-rope1/cosma7/SWIFT/DMO_1380/data/ani_hydro_' + snap + ".hdf5"
 
     snap = "%05d" % num
 
