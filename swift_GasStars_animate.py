bae4e4b56a7cf3a1c233df4dc27970871d0e3d07
wjr21 <wjr21@sussex.ac.uk>
2020-11-11 15:15:55 +0000

    angled cube

--- swift_GasStars_animate.py
+++ swift_GasStars_animate.py
@@ -131,5 +131,5 @@ if len(sys.argv) > 1:
 else:
 
     for num in range(0, 1001):
-        single_frame(num, max_pixel=5.5, nframes=1380)
+        single_frame(num, max_pixel=6.5, nframes=1380)
         gc.collect()
