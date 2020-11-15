6744b23bdbc12f6cf2124a515a26a61269d52efd
wjr21 <wjr21@sussex.ac.uk>
2020-11-15 22:26:22 +0000

    angled cube

--- swift_GasStars_flythrough.py
+++ swift_GasStars_flythrough.py
@@ -79,7 +79,7 @@ def single_frame(num, max_pixel, nframes):
     print(boxsize)
 
     # Define centre
-    cent = np.array([12.1, 4.1, 2])
+    cent = np.array([12.1, 4.1, 1.5])
     
     # Define targets
     targets = [[0, 0, 0]]
