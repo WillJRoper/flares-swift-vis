a7cedbabbdede8b803c34d83576d9e668c55fafb
wjr21 <wjr21@sussex.ac.uk>
2020-11-16 22:37:51 +0000

    angled cube

--- swift_GasStars_flythrough.py
+++ swift_GasStars_flythrough.py
@@ -87,9 +87,9 @@ def single_frame(num, max_pixel, nframes):
     # Define anchors dict for camera parameters
     anchors = {}
     anchors['sim_times'] = [0.0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
-    anchors['id_frames'] = [0, 120, 180, 500, 900, 1000, 1200, 1379]
+    anchors['id_frames'] = [0, 120, 150, 500, 900, 1000, 1200, 1379]
     anchors['id_targets'] = [0, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
-    anchors['r'] = [boxsize.value + 5, 'pass', 1, 'same', 'same', 'pass', 'pass', boxsize.value + 5]
+    anchors['r'] = [boxsize.value + 5, 'pass', 1.5, 'same', 'same', 'pass', 'pass', boxsize.value + 5]
     anchors['t'] = [5, 'same', 'same', 'same', 'same', 'same', 'same', 'same']
     anchors['p'] = [0, 'same', 'same', 'pass', 'pass', 'pass', 'pass', -360]
     anchors['zoom'] = [1., 'same', 'same', 'same', 'same', 'same', 'same', 'same']
