f28cb55d014545534e0fca090fe857837548245d
wjr21 <wjr21@sussex.ac.uk>
2020-11-22 23:48:37 +0000

    angled cube

--- swift_GasStars_flythrough.py
+++ swift_GasStars_flythrough.py
@@ -54,7 +54,7 @@ def getimage(data, poss, mass, hsml, num, max_pixel, cmap, Type="gas"):
         print("gas", np.max(img))
     else:
         vmax = 12
-        vmin = 7
+        vmin = 7.5
         print("star", np.max(img))
 
     # Convert images to rgb arrays
