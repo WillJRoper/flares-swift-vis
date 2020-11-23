ee7952dc10f0b89e8937e74d9f66c3736e5b371c
wjr21 <wjr21@sussex.ac.uk>
2020-11-23 11:55:14 +0000

    angled cube

--- swift_GasTemp_flythrough.py
+++ swift_GasTemp_flythrough.py
@@ -193,9 +193,9 @@ def single_frame(num, max_pixel, nframes):
     sm._A = []  # # fake up the array of the scalar mappable
     cbaxes = ax.inset_axes([0.05, 0.95, 0.3, 0.02])
     cbar = plt.colorbar(sm, cax=cbaxes, orientation="horizontal")
-    cbar.ax.set_xticks([3, 4, 5, 6, 7, 8])
+    cbar.set_xticks([3, 4, 5, 6, 7, 8])
     labels = [item.get_text() for item in cbar.ax.get_xticklabels()]
-    labels[-1] = "$8\geq$"
+    labels[-1] = "$8\leq$"
     cbar.ax.set_xticklabels(labels)
     for tick in cbar.ax.xaxis.get_major_ticks():
         tick.label.set_fontsize(3)
