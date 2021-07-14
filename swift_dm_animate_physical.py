9932234170fa9fe0f59308c2cd5096cfb0350e18
wjr21 <wjr21@sussex.ac.uk>
2021-07-14 10:14:08 +0100

    physical animation

--- swift_dm_animate_physical.py
+++ swift_dm_animate_physical.py
@@ -56,7 +56,7 @@ def getimage(data, poss, hsml, num, z):
           np.percentile(img, 67.5),
           np.percentile(img, 50))
 
-    vmax = 6
+    vmax = 5.5
     vmin = 1
 
     # # Get colormaps
