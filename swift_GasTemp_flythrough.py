c23b72a8c079b487b50bce8501f76ba753dca4a1
wjr21 <wjr21@sussex.ac.uk>
2021-07-20 15:28:52 +0100

    recession animation

--- swift_GasTemp_flythrough.py
+++ swift_GasTemp_flythrough.py
@@ -263,10 +263,10 @@ def single_frame(num, max_pixel, nframes):
     for tick in cbar.ax.xaxis.get_major_ticks():
         tick.label.set_fontsize(0.1)
         tick.label.set_color("w")
-        tick.label.set_y(10)
+        tick.label.set_y(5)
     cbar.ax.tick_params(axis='x', color='w', size=0.3, width=0.1)
     cbar.ax.set_xlabel("$\log_{10}(T / [\mathrm{K}])$", color='w',
-                       fontsize=0.4, labelpad=-7.5)
+                       fontsize=0.4, labelpad=-2)
     cbar.outline.set_edgecolor('white')
     cbar.outline.set_linewidth(0.05)
 
