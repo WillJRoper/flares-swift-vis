50f76cbe6b54a955dbc6d420d562f64ade856f6d
wjr21 <wjr21@sussex.ac.uk>
2021-07-26 02:48:53 +0100

    360 image cube maps

--- swift_GasStars_flythrough_360.py
+++ swift_GasStars_flythrough_360.py
@@ -266,14 +266,16 @@ def single_frame(num, max_pixel, nframes):
     cart_poss = np.copy(poss)
     poss = cart_to_spherical(poss)
     print(poss.min(axis=0), poss.max(axis=0))
-    poss, rs = spherical_to_equirectangular(poss)
-    print(poss.min(axis=0), poss.max(axis=0))
+    # poss, rs = spherical_to_equirectangular(poss)
+    # print(poss.min(axis=0), poss.max(axis=0))
 
     max_rad = np.sqrt(3 * (boxsize.value / 2)**2)
 
     # Define range and extent for the images in arc seconds
-    imgrange = ((max_rad * -np.pi, max_rad * np.pi),
-                (max_rad * -np.pi / 2, max_rad * np.pi / 2))
+    # imgrange = ((max_rad * -np.pi, max_rad * np.pi),
+    #             (max_rad * -np.pi / 2, max_rad * np.pi / 2))
+    imgrange = ((poss[:, 0].min(), poss[:, 0].max()),
+                (poss[:, 1].min(), poss[:, 1].max()))
     imgextent = (max_rad * -np.pi, max_rad * np.pi,
                  max_rad * -np.pi / 2, max_rad * np.pi / 2)
 
