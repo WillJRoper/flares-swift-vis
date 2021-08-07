9580fd572199e4e5c13fb6b2eb22eb279abe1d30
wjr21 <wjr21@sussex.ac.uk>
2021-08-07 23:51:07 +0100

    wrapped physical

--- swift_dm_animate_physical_wrapped.py
+++ swift_dm_animate_physical_wrapped.py
@@ -178,15 +178,16 @@ def single_frame(num, max_pixel, nframes):
 
     wrapped_boxes = int(np.ceil(1 + z))
     half_wrapped_boxes = int(wrapped_boxes / 2)
-    wrapped_poss = np.zeros((wrapped_boxes ** 3, 3))
-    wrapped_hsmls = np.zeros((wrapped_boxes ** 3, 3))
+    wrapped_poss = np.zeros((poss.shape[0] * wrapped_boxes ** 3, 3))
+    wrapped_hsmls = np.zeros((poss.shape[0] * wrapped_boxes ** 3, 3))
     print(wrapped_poss.shape)
     n = 0
     for i in range(-half_wrapped_boxes, half_wrapped_boxes, 1):
         for j in range(-half_wrapped_boxes,half_wrapped_boxes, 1):
             for k in range(-half_wrapped_boxes, half_wrapped_boxes, 1):
-                wrapped_poss[poss.shape * n: poss.shape * (n + 1), 3] = poss + np.array([i * boxsize / (1 + z), j * boxsize / (1 + z), k * boxsize / (1 + z)])
-                wrapped_hsmls[poss.shape * n: poss.shape * (n + 1)] = hsmls
+                wrapped_poss[poss.shape[0] * n: poss.shape[0] * (n + 1), 3] = poss + np.array([i * boxsize / (1 + z), j * boxsize / (1 + z), k * boxsize / (1 + z)])
+                wrapped_hsmls[poss.shape[0] * n: poss.shape[0] * (n + 1)] = hsmls
+                n += 1
 
     # Get images
     cmap = cmr.sepia
